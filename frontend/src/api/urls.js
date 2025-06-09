30764597bb68f2d0b929f3a927db2dcfcb7be983
YasiruRavidith <yasiruravidith123@gmail.com>
2025-06-09 12:36:41 +0530

    lan hositng

--- frontend/src/api/urls.js
+++ frontend/src/api/urls.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
-const WS_BASE_URL = 'ws://127.0.0.1:8000';
+const API_BASE_URL = 'http://192.168.1.104:8000/api';
+const WS_BASE_URL = 'ws://192.168.1.104:8000';
 
 export const API_URLS = {
     BASE: API_BASE_URL,
